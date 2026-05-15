eedc10d18d053a03e4701f84ceea0bb19d6f7aa3
ryanmoraes2216 <ryanmoraes1622@gmail.com>
2026-05-15 12:56:53 -0300

    Add eleição.c - Voter eligibility checker program

---
+++ eleição.c
@@ -0,0 +1,10 @@
+int idades[5] = {18, 19, 13, 22, 16};
+int i;
+for(i = 0; i <5; i++) {
+if(idades[i] >=18) {
+printf("pode tirar titulo de eleitor\n");
+ }
+else { 
+printf("não pode tirar titulo de eleitor\n"); 
+}
+}
